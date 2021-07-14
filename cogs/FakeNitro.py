4e5f85c46c94d04ec34ea463c04b1db188183ce8
timothytt547 <timlau.cy@gmail.com>
2021-07-14 23:10:37 +1000

    Added /de to delete a user's previous /fn usage

--- cogs/FakeNitro.py
+++ cogs/FakeNitro.py
@@ -4,6 +4,7 @@ import requests
 # discord libraries
 import discord
 from discord import Guild
+from collections import deque
 
 from discord.ext import commands
 
@@ -19,14 +20,13 @@ from re import search
 # bl_servers = ["ohhh"]
 bl_servers = []
 
-async def nuke_messages(msgs, *args):
-    if len(args) > 0:
-        time.sleep(int(args[0]))
-    for m in msgs:
-        await m.delete()
-
 def ret_e_name(e):
     return str(e.name)
+
+# invoked msgs dict,
+# key = invoked user id
+# value = the command message (from the bot) from that user (id only?)
+invo_msgs = {}
 
 class FakeNitro(commands.Cog):
     def __init__(self, bot):
@@ -40,19 +40,19 @@ class FakeNitro(commands.Cog):
                         description="Emote name to search for",
                         option_type=3,
                         required=True
+                    ),
+                    create_option(
+                        name="large",
+                        description="Larger version of the emote?",
+                        option_type=5,
+                        required=False
                     )
                 ])
-    async def fn(self, ctx, name:str):
-        msgs = []
-
-        # if len(args) == 0:
-        #     msgs.append(await ctx.send("Input an emote name to search for."))
+    async def fn(self, ctx, name:str, large:bool=False):
         if len(name) < 3:
-            msgs.append(await ctx.send("Search term must be longer than 2 letters."))
+            await ctx.send("Search term must be longer than 2 letters.", delete_after=3)
         else:
-            atta = False
-            max_similar = 0
-            max_e = ""
+            found = False
             async for guild in self.bot.fetch_guilds():
                 g = await self.bot.fetch_guild(guild.id)
                 if g.name in bl_servers:
@@ -61,27 +61,45 @@ class FakeNitro(commands.Cog):
                 for e in g.emojis:
                     # print(args[0].lower() + " " + e.name.lower())
                     if search(name.lower(), e.name.lower()):
-                        atta = True
-                        # await ctx.send((ctx.author.nick if ctx.author.nick else str(ctx.author)[:len(str(ctx.author))-5])+":")
-                        await ctx.send(str(e))
+                        found = True
+                        if large:
+                            m = await ctx.send("https://cdn.discordapp.com/emojis/"+str(e.id)+".png")
+                        else:
+                            m = await ctx.send(str(e))
+                        invo_msgs[str(ctx.author.id)] = m
                         break
-                if atta:
+                if found:
                     break
 
-            if not atta:
-                msgs.append(await ctx.send("No similar matching emotes found, try harder or use /fnlist for a full list."))
+            if not found:
+                # msgs.append(await ctx.send("No similar matching emotes found, try harder or use /fnlist for a full list.", delete_after=3))
+                await ctx.send("No similar matching emotes found, try harder or use /fnlist for a full list.", delete_after=3)
 
-        await nuke_messages(msgs, 3)
+
+    @cog_ext.cog_slash(name="de", description="Deletes the previous emote you sent with the bot")
+    async def de(self, ctx):
+        # print("Author: " + str(ctx.author.id) + ", Message: " + str(m.id))
+        if str(ctx.author.id) in invo_msgs:
+            m = invo_msgs[str(ctx.author.id)]
+            if m:
+                invo_msgs[str(ctx.author.id)] = None
+                await m.delete()
+                await ctx.send("Deleted.",delete_after=.001)
+            else:
+                await ctx.send("Can only delete immediate previous emote.", delete_after=3)
+        else:
+            await ctx.send("No previous /fn usage recorded from you.", delete_after=3)
+
 
     @cog_ext.cog_slash(name="fnlist", description="DM's you a list of available emotes")
     async def fnlist(self, ctx):
         # """DM's you a list of available emotes
         # By default sends the max amount of emotes per message, which condenses the amount of messages but loses large emote viewing. To preview with larger emotes, use "$fnlist large".
         # """
+
+        await ctx.send("Sending...", delete_after=.001)
         e_send_count = 100
         char_count = 0
-
-
 
         async for guild in self.bot.fetch_guilds():
             g = await self.bot.fetch_guild(guild.id)
@@ -112,6 +130,7 @@ class FakeNitro(commands.Cog):
                 await ctx.author.send(e_string)
 
         await ctx.author.send("End of list")
+
 
 def setup(bot):
     bot.add_cog(FakeNitro(bot))
